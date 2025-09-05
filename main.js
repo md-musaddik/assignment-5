e8b6a00c171333300bd109aded92470d66a525d3
md-musaddik <musaddikibna1@gmail.com>
2025-09-05 21:43:35 +0600

    added call feature

--- main.js
+++ main.js
@@ -9,3 +9,37 @@ for(let heart of hearts){
     })
 }
 
+//call button functionalities
+let callButtons=document.getElementsByClassName("call");
+
+for(let callBtn of callButtons){
+    callBtn.addEventListener("click",function(){
+        let serviceName=callBtn.parentNode.parentNode.children[1].innerText;
+        let serviceNum=callBtn.parentNode.parentNode.children[3].innerText;
+        let historyParent=document.getElementById("history-section");
+        let coins=parseInt(document.getElementById("coins").innerText);
+        let date=new Date().toLocaleTimeString();
+        if(coins>=20){
+            alert("Calling "+ serviceName+ " "+serviceNum+"...");
+            coins-=20;
+            document.getElementById("coins").innerText=coins
+            historyParent.innerHTML+=`
+                <div class="d-flex addedDiv">
+                    <div>
+                    <p style="font-weight:bold;">${serviceName}</p>
+                    <p>${serviceNum}</p>
+                    </div>
+                    <p>${date}</p>
+                </div>
+            `;
+
+            
+        }else{
+            alert("You don't have enough coins to Call")
+        }
+    })
+}
+
+//clear button
+document.getElementById()
+
